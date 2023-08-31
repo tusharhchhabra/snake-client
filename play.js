992c4f09c50190868d25d5eb1be49e7ac5fc3e75
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-31 00:51:48 -0700

    stdin setup

--- play.js
+++ play.js
@@ -14,3 +14,23 @@ conn.on("connect", () => {
 conn.on("data", (message) => {
   console.log(message);
 });
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+
+  stdin.on("data", (data) => handleUserInput(data));
+
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+setupInput();
