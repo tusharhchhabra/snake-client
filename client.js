66cee722f7d4c02a888729f48b42547148ba3110
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-31 01:12:21 -0700

    Constants module

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require("./constants")
 
-// establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   conn.setEncoding("utf8");
@@ -12,8 +12,6 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Connected to the game server.");
     conn.write("Name: TUS");
-  
-    // conn.write("Move: up");
   });
   
   conn.on("data", (message) => {
--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,9 +14,26 @@ const setupInput = function () {
 };
 
 const handleUserInput = (key) => {
-  if (key === '\u0003') {
+  if (key === "\u0003") {
     process.exit();
   }
-}
 
-module.exports = { setupInput }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+
+  if (key === "y") {
+    connection.write("Say: yay!");
+  }
+};
+
+module.exports = { setupInput };
